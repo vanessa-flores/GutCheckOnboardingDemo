67d134a38cf437404f5da3e63530e70954642958
Vanessa Flores <risingdevhabits@gmail.com>
2025-12-19 15:14:17 -0600

    Create GettingStartedViewModel and GettingStartedRouter

---
+++ GutCheckOnboardingDemo/Getting Started/GettingStartedViewModel.swift
@@ -0,0 +1,118 @@
+import SwiftUI
+import Observation
+
+@Observable
+class GettingStartedViewModel {
+    
+    // Navigation
+    var router: GettingStartedRouter
+    
+    // Animation State
+    var contentOffset: CGFloat = 0
+    var showContent: Bool = true
+    let screenWidth = UIScreen.main.bounds.width
+    
+    // MARK: - Private Properties
+    
+    private let onComplete: () -> Void
+    
+    // MARK: - Computed Properties
+    
+    var progressIndex: Int {
+        router.activeScreen.progressIndex
+    }
+    
+    var screenCount: Int {
+        return 4
+    }
+    
+    var canGoBack: Bool {
+        router.canGoBack
+    }
+    
+    // MARK: - Button Configuration
+
+    var primaryButtonTitle: String {
+        switch router.activeScreen {
+        case .goalsMotivations: return GettingStartedCopy.GoalsAndMotivations.buttonTitle
+        case .gutHealthAwareness: return GettingStartedCopy.GutHealthAwareness.buttonTitle
+        case .menstrualCycleStatus: return GettingStartedCopy.CurrentCycleStatus.buttonTitle
+        case .symptomSelection: return GettingStartedCopy.SymptomSelection.buttonTitle
+        }
+    }
+    
+    // MARK: - Init
+    
+    init(router: GettingStartedRouter = GettingStartedRouter(), onComplete: @escaping () -> Void) {
+        self.router = router
+        self.onComplete = onComplete
+    }
+    
+    // MARK: - Actions
+    
+    func handlePrimaryAction() {
+        switch router.activeScreen {
+        case .symptomSelection:
+            handleSymptomSelection()
+        default:
+            animateForward { [weak self] in
+                self?.router.goToNextScreen()
+            }
+        }
+    }
+    
+    func goBack() {
+        animateBackward { [weak self] in
+            self?.router.goBack()
+        }
+    }
+    
+    func completeGettingStarted() {
+        onComplete()
+    }
+    
+    // MARK: - Animations
+    
+    private func animateForward(completion: @escaping () -> Void) {
+        // Slide current content out to left
+        withAnimation(.easeOut(duration: 0.2)) {
+            contentOffset = -screenWidth
+        }
+
+        // After slide out, change screen and prepare new content
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+            guard let self else { return }
+            completion()
+            self.contentOffset = self.screenWidth
+
+            // Slide new content in from right
+            withAnimation(.easeOut(duration: 0.2)) {
+                self.contentOffset = 0
+            }
+        }
+    }
+    
+    private func animateBackward(completion: @escaping () -> Void) {
+        // Slide current content out to right
+        withAnimation(.easeOut(duration: 0.2)) {
+            contentOffset = screenWidth
+        }
+
+        // After slide out, change screen and prepare new content
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+            guard let self else { return }
+            completion()
+            self.contentOffset = -self.screenWidth
+
+            // Slide new content in from left
+            withAnimation(.easeOut(duration: 0.2)) {
+                self.contentOffset = 0
+            }
+        }
+    }
+    
+    private func handleSymptomSelection() {
+        print("Saving selected symptoms...")
+        completeGettingStarted()
+    }
+}
