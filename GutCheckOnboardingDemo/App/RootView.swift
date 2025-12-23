e2fda3bb27cd35b57d5c412aaa17aca2866845d3
Vanessa Flores <risingdevhabits@gmail.com>
2025-12-23 11:21:32 -0600

    Refactor: Extract MainAppView into separate file

--- GutCheckOnboardingDemo/App/RootView.swift
+++ GutCheckOnboardingDemo/App/RootView.swift
@@ -29,31 +29,6 @@ struct RootView: View {
     }
 }
 
-// MARK: - Main App View
-
-struct MainAppView: View {
-    @State var appRouter: AppRouter
-
-    var body: some View {
-        TabView(selection: $appRouter.mainAppRouter.selectedTab) {
-            DashboardView(appRouter: appRouter)
-                .tabItem {
-                    Label(
-                        appRouter.mainAppRouter.selectedTab.title,
-                        systemImage: appRouter.mainAppRouter.selectedTab.icon
-                    )
-                }
-                .tag(MainTab.dashboard)
-            
-            LoggingContainerView(userId: appRouter.currentUserId, appRouter: appRouter)
-                .tabItem {
-                    Label(MainTab.log.title, systemImage: MainTab.log.icon)
-                }
-                .tag(MainTab.log)
-        }
-    }
-}
-
 #Preview {
     RootView()
 }
