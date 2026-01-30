f019b03842905400818b9e7f609d8ad01539328d
Vanessa Flores <risingdevhabits@gmail.com>
2026-01-30 15:44:42 -0600

    Extract FlowLayout to shared component

--- GutCheckOnboardingDemo/LoggingHub/DailyCheckIn/Child Views/SymptomsTabView.swift
+++ GutCheckOnboardingDemo/LoggingHub/DailyCheckIn/Child Views/SymptomsTabView.swift
@@ -92,60 +92,6 @@ struct SymptomsTabView: View {
     }
 }
 
-// MARK: - FlowLayout
-
-struct FlowLayout: Layout {
-    var spacing: CGFloat
-    
-    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-        let result = FlowResult(in: proposal.replacingUnspecifiedDimensions().width, subviews: subviews, spacing: spacing)
-        return result.size
-    }
-    
-    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        let result = FlowResult(in: bounds.width, subviews: subviews, spacing: spacing)
-        for (index, subview) in subviews.enumerated() {
-            subview.place(at: CGPoint(x: bounds.minX + result.frames[index].minX, y: bounds.minY + result.frames[index].minY), proposal: .unspecified)
-        }
-    }
-    
-    struct FlowResult {
-        var frames: [CGRect] = []
-        var size: CGSize = .zero
-        
-        init(in maxWidth: CGFloat, subviews: Subviews, spacing: CGFloat) {
-            var currentX: CGFloat = 0
-            var currentY: CGFloat = 0
-            var lineHeight: CGFloat = 0
-            
-            for subview in subviews {
-                let size = subview.sizeThatFits(.unspecified)
-                
-                if currentX + size.width > maxWidth && currentX > 0 {
-                    currentX = 0
-                    currentY += lineHeight + spacing
-                    lineHeight = 0
-                }
-                
-                frames.append(CGRect(origin: CGPoint(x: currentX, y: currentY), size: size))
-                lineHeight = max(lineHeight, size.height)
-                currentX += size.width + spacing
-            }
-            
-            self.size = CGSize(width: maxWidth, height: currentY + lineHeight)
-        }
-    }
-}
-
-// MARK: - PlainHeaderButtonStyle
-
-struct PlainHeaderButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .contentShape(Rectangle())
-    }
-}
-
 // MARK: - Previews
 
 #Preview("Empty State") {
