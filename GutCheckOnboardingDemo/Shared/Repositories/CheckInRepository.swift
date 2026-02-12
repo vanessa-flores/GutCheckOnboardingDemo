10ecf579058742ed50433ec80f438091a64097a0
Vanessa Flores <risingdevhabits@gmail.com>
2026-02-12 17:23:12 -0600

    Refactor TodaysCheckInViewModel to use SymptomFilteringService

--- GutCheckOnboardingDemo/Shared/Repositories/CheckInRepository.swift
+++ GutCheckOnboardingDemo/Shared/Repositories/CheckInRepository.swift
@@ -1,7 +1,6 @@
 import Foundation
 
-protocol CheckInRepositoryProtocol {
+protocol CheckInRepositoryProtocol: SymptomCatalogProtocol {
     func dailyLog(for userId: UUID, on: Date) -> DailyLog?
     func save(dailyLog: DailyLog)
-    func symptomsGroupedByCategory() -> [(category: SymptomCategory, symptoms: [Symptom])]
 }
